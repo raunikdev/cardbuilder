c6e4a5446e27e9218bab42eb9f889757ee1ed3c3
Raunik Rai <raunik.rai@amptech.corp>
2025-07-25 20:38:18 +0530

    Updated UI or feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Cardbuilder from './components/Cardbuilder'
+import { Analytics } from "@vercel/analytics/next"
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Cardbuilder/>
+      <Analytics/>
     </>
   )
 }
