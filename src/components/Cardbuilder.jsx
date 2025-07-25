6a5cf54e122ff916f64b1d06705d0b6427819d91
Raunik Rai <raunik.rai@amptech.corp>
2025-07-25 22:29:35 +0530

    default value for button text color

--- src/components/Cardbuilder.jsx
+++ src/components/Cardbuilder.jsx
@@ -1,154 +1,156 @@
 import './css/cardbuilder.css'
-import {useState} from 'react'
+import { useState } from 'react'
 import Pic from './img/profile-default.jpg';
 import Instagramicon from './img/instagram-icon.png';
 import Linkedinicon from './img/linkedin-icon.png';
 import Youtubeicon from './img/youtube-icon.png';
 import Xicon from './img/x-icon.png';
-function Cardbuilder(){
-    const [bgcolor,Setbgcolor] = useState("rgb(255,229,229)");
-    const [name,Setname] = useState("Your Name...");
-    const [description,Setdescription] = useState("I am good at html well better in css good for me i guess");
-    const [textcolor,Settextcolor] = useState("black");
-    const [instalink,Setinstalink] = useState("");
-    const [linkedinlink,Setlinkedinlink] = useState("");
-    const [youtubelink,Setyoutubelink] = useState("");
-    const [xlink,Setxlink] = useState("");
-    const [buttonbgcolor,Setbgbuttoncolor] = useState("");
-    const [buttoncolor,Setbuttoncolor] = useState("");
-    const [viewcv,Setviewcv] = useState("");
-    const [emailme,Setemailme] = useState("");
-    
-    const bgcolorChangeHandler =(e)=>{
+function Cardbuilder() {
+    const [bgcolor, Setbgcolor] = useState("rgb(255,229,229)");
+    const [name, Setname] = useState("Your Name...");
+    const [description, Setdescription] = useState("I am good at html well better in css good for me i guess");
+    const [textcolor, Settextcolor] = useState("black");
+    const [instalink, Setinstalink] = useState("");
+    const [linkedinlink, Setlinkedinlink] = useState("");
+    const [youtubelink, Setyoutubelink] = useState("");
+    const [xlink, Setxlink] = useState("");
+    const [buttonbgcolor, Setbgbuttoncolor] = useState("");
+    const [buttoncolor, Setbuttoncolor] = useState("#000000");
+    const [viewcv, Setviewcv] = useState("");
+    const [emailme, Setemailme] = useState("");
+
+    console.log(buttoncolor, "buttonColor");
+
+    const bgcolorChangeHandler = (e) => {
         Setbgcolor(e.target.value);
     }
-    const nameHandler=(event)=>{
+    const nameHandler = (event) => {
         Setname(event.target.value)
     }
-    const descriptionHandler=(e)=>{
+    const descriptionHandler = (e) => {
         Setdescription(e.target.value)
     }
-    const textcolorHandler=(event)=>{
+    const textcolorHandler = (event) => {
         Settextcolor(event.target.value)
     }
-    const instalinkHandler=(e)=>{
+    const instalinkHandler = (e) => {
         Setinstalink(e.target.value);
     }
-    const linkedinlinkHandler=(e)=>{
+    const linkedinlinkHandler = (e) => {
         Setlinkedinlink(e.target.value);
     }
-    const youtubelinkHandler=(e)=>{
+    const youtubelinkHandler = (e) => {
         Setyoutubelink(e.target.value);
     }
-    const xlinkHandler =(e)=>{
+    const xlinkHandler = (e) => {
         Setxlink(e.target.value);
     }
-    const buttonbgcolorHandler=(e)=>{
+    const buttonbgcolorHandler = (e) => {
         Setbgbuttoncolor(e.target.value);
     }
-    const buttoncolorHandler=(e)=>{
+    const buttoncolorHandler = (e) => {
         Setbuttoncolor(e.target.value);
     }
-    const viewcvHandler =(e)=>{
+    const viewcvHandler = (e) => {
         Setviewcv(e.target.value);
     }
-    const emailmeHandler =(e)=>{
+    const emailmeHandler = (e) => {
         Setemailme(e.target.value);
     }
-    return(
+    return (
         <>
             <h1>Card Builder: </h1>
             <div className="builder-all">
                 <div className="builder-left">
-                    <h3 style={{display: "inline-block"}}>Select your Background Color: &nbsp;</h3>
-                    <input className="color-selection" type="color" value={bgcolor} onChange={bgcolorChangeHandler}/>
-                    
-                    
-                    <h3 style={{display: "inline-block"}}>Select your Text Color:&nbsp; </h3>
+                    <h3 style={{ display: "inline-block" }}>Select your Background Color: &nbsp;</h3>
+                    <input className="color-selection" type="color" value={bgcolor} onChange={bgcolorChangeHandler} />
+
+
+                    <h3 style={{ display: "inline-block" }}>Select your Text Color:&nbsp; </h3>
                     <input className="color-selection" type="color" value={textcolor} onChange={textcolorHandler}></input>
                     {/* <input type="file" className='image-exceptor'/> */}
-                    
-                    
-                    <input type="text" value={name} className='name-acceptor' placeholder='Your Name...' onChange={nameHandler}/>
-                    
+
+
+                    <input type="text" value={name} className='name-acceptor' placeholder='Your Name...' onChange={nameHandler} />
+
                     <h5>Description:</h5>
-                    <textarea type="text" value={description} rows="3" className='description-acceptor' onChange={descriptionHandler}/>
-                    
-                    
+                    <textarea type="text" value={description} rows="3" className='description-acceptor' onChange={descriptionHandler} />
+
+
                     <h3>Social Media Handles (Links):</h3>
                     <div className='social-input'>
-                        <img className="socialmedia-icons" src={Instagramicon}/>
+                        <img className="socialmedia-icons" src={Instagramicon} />
                         <input className='socialmedia-input' value={instalink} placeholder="Insta profile url..." onChange={instalinkHandler}></input>
                     </div>
                     <div className='social-input'>
 
-                        <img className="socialmedia-icons" src={Linkedinicon}/>
+                        <img className="socialmedia-icons" src={Linkedinicon} />
                         <input className='socialmedia-input' value={linkedinlink} placeholder='Linkedin profile url..' onChange={linkedinlinkHandler}></input>
                     </div>
                     <div className='social-input'>
-                        <img className="socialmedia-icons" src={Youtubeicon}/>
+                        <img className="socialmedia-icons" src={Youtubeicon} />
                         <input className='socialmedia-input' value={youtubelink} placeholder='Youtube profile url..' onChange={youtubelinkHandler}></input>
 
                     </div>
                     <div className='social-input'>
-                        <img className="socialmedia-icons" src={Xicon}/>
+                        <img className="socialmedia-icons" src={Xicon} />
                         <input className='socialmedia-input' value={xlink} placeholder='Twitter profile url..' onChange={xlinkHandler}></input>
 
                     </div>
-                    
-                    <h4 style={{display: "inline-block"}}>Select your Button Background Color: &nbsp;</h4>
-                    <input className="color-selection" type="color" value={buttonbgcolor} onChange={buttonbgcolorHandler}/>
-                    <h4 style={{display: "inline-block"}}>Select your Button Text Color:&nbsp; </h4>
+
+                    <h4 style={{ display: "inline-block" }}>Select your Button Background Color: &nbsp;</h4>
+                    <input className="color-selection" type="color" value={buttonbgcolor} onChange={buttonbgcolorHandler} />
+                    <h4 style={{ display: "inline-block" }}>Select your Button Text Color:&nbsp; </h4>
                     <input className="color-selection" type="color" value={buttoncolor} onChange={buttoncolorHandler}></input>
 
-                    <input placeholder="CV URL...." type="text" value={viewcv} className='cv-email-input' onChange={viewcvHandler}/>
-                    <input placeholder="Email Id...." type="email" value={emailme} className='cv-email-input' onChange={emailmeHandler}/>
+                    <input placeholder="CV URL...." type="text" value={viewcv} className='cv-email-input' onChange={viewcvHandler} />
+                    <input placeholder="Email Id...." type="email" value={emailme} className='cv-email-input' onChange={emailmeHandler} />
                 </div>
                 <div className="cardbuild-right">
-                    <div className="card" style={{backgroundColor: bgcolor,color: textcolor}}>
-                        <img src={Pic} className='profile-pic'/>
-                        
+                    <div className="card" style={{ backgroundColor: bgcolor, color: textcolor }}>
+                        <img src={Pic} className='profile-pic' />
+
                         <b><p className='name'>{name}</p></b>
-                        
+
                         <p className="description">{description}</p>
-                        
-                        
+
+
                         <div className="socialhandles-line">
                             <a href={instalink} target='_blank'>
-                                <img className="socialmedia-icons" style={{display: instalink===""?"none":"inline-block"}} src={Instagramicon}/>
-                                
+                                <img className="socialmedia-icons" style={{ display: instalink === "" ? "none" : "inline-block" }} src={Instagramicon} />
+
                             </a>
                             <a href={linkedinlink} target="_blank">
-                                <img className="socialmedia-icons" style={{display: linkedinlink===""?"none":"inline-block"}} src={Linkedinicon}/>
+                                <img className="socialmedia-icons" style={{ display: linkedinlink === "" ? "none" : "inline-block" }} src={Linkedinicon} />
                             </a>
                             <a href={youtubelink} target="_blank">
-                                <img className="socialmedia-icons" style={{display: youtubelink===""?"none":"inline-block"}} src={Youtubeicon}/>
+                                <img className="socialmedia-icons" style={{ display: youtubelink === "" ? "none" : "inline-block" }} src={Youtubeicon} />
                             </a>
-                            
+
                             <a href={xlink} target="_blank">
-                                <img className="socialmedia-icons" style={{display: xlink===""?"none":"inline-block"}} src={Xicon}/>
+                                <img className="socialmedia-icons" style={{ display: xlink === "" ? "none" : "inline-block" }} src={Xicon} />
                             </a>
                         </div>
                         <div className="cv-email">
                             <a href={viewcv} >
-                                <button 
+                                <button
                                     className='view-cv'
-                                    style={{backgroundColor: buttonbgcolor, color: buttoncolor}}
+                                    style={{ backgroundColor: buttonbgcolor, color: buttoncolor }}
                                     onChange={viewcvHandler}
                                     target="_blank"
-                                    >View CV</button>
+                                >View CV</button>
                             </a>
                             <a href={`mailto:${emailme}`}>
-                                <button 
-                                    className="email-me" 
-                                    style={{backgroundColor: buttonbgcolor, color: buttoncolor}}
+                                <button
+                                    className="email-me"
+                                    style={{ backgroundColor: buttonbgcolor, color: buttoncolor }}
                                     onChange={emailmeHandler}
                                     target="_blank"
-                                    >Email Me</button>
+                                >Email Me</button>
                             </a>
                         </div>
-                        
-                        
+
+
                     </div>
                 </div>
             </div>
